cebf6435ecd7d41865aad75b552d1de23cfebbba
zikra03 <114687288+zikra03@users.noreply.github.com>
2024-09-29 12:36:23 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+// import 'package:workout_fitness/view/login/on_boarding_view.dart';
+//import 'package:workout_fitness/view/menu/menu_view.dart';
+
+import 'common/color_extension.dart';
+import 'view/login/on_boarding_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Workout Fitness',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Quicksand",
+        colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
+        useMaterial3: false,
+      ),
+      home: const OnBoardingView(),
+    );
+  }
+}
+
+
